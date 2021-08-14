cd30e235032d913226b9b763199bc2c6a37269e5
Sairam-2189 <88224231+Sairam-2189@users.noreply.github.com>
2021-08-14 17:02:53 +0530

    no changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 const cityList = [
-["Goa","India"],
-["Amsterdam","Netherlands"],
-["New York","USA"],
-["Darjeeling","India"],
-["Tokyo","Japan"],
-["Lonavala","India"]
+  ["Goa", "India"],
+  ["Amsterdam", "Netherlands"],
+  ["New York", "USA"],
+  ["Darjeeling", "India"],
+  ["Tokyo", "Japan"],
+  ["Lonavala", "India"]
 ];
 //Goa(India), Amsterdam(Netherlands), New York(USA), Darjeeling(India), Tokyo(Japan), Lonavala(India)
 
 const indiaCities = cityList.filter((pair) => {
- return pair[1] == "India"; 
-})
+  return pair[1] == "India";
+});
 
 export default function App() {
   return (
     <ol>
-      {indiaCities.map((pair,i) => {
-        return <li key = { "location"+(i+1)}>{pair[0]}</li>
-      }
-      )}
+      {indiaCities.map((pair, i) => {
+        return <li key={"location" + (i + 1)}>{pair[0]}</li>;
+      })}
     </ol>
   );
 }
